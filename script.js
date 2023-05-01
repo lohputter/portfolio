8e2a5adda2331a41b0b714e20c108ae64c5d9c06
lohputter <129501406+lohputter@users.noreply.github.com>
2023-05-01 23:32:15 +0800

    YES

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ function showQuote() {
         }
     }, 50);
 }
+setInterval(function() {
+    document.getElementsByClassName('bg-code')[Math.floor(Math.random() * document.getElementsByClassName("bg-code").length)].innerHTML += Math.floor(Math.random() * 2);
+    for (let i=0; i<document.getElementsByClassName("bg-code").length; i++) {
+        if (document.getElementsByClassName("bg-code")[i].clientWidth >= document.getElementById("cca-title").clientWidth) {
+            document.getElementsByClassName("bg-code")[i].innerHTML = "X".repeat(document.getElementsByClassName("bg-code")[i].innerHTML.length);
+            document.getElementsByClassName("bg-code")[i].style.opacity = 0.5;
+            setTimeout(function() {
+                document.getElementsByClassName("bg-code")[i].style.opacity = 1;
+                document.getElementsByClassName("bg-code")[i].innerHTML = "";
+            }, 100); 
+        } 
+    }
+}, 20);
